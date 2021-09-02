5d84233dbb1d756333040409a193aa3d27aa0193
zzj <88833182+onlyzzj@users.noreply.github.com>
2021-09-02 20:13:02 +0800

    Add files via upload

---
+++ 12864液晶时钟/lcd12864.h
@@ -0,0 +1,21 @@
+#ifndef  __lcd12864_h_
+#define  __lcd12864_h_
+#include "common.h"
+
+
+#define DATAPORT P0
+sbit rs=P3^5;
+sbit rw=P3^6;
+sbit en=P3^7;
+sbit PSB=P3^1;
+//sbit beiguang=P3^7;
+
+
+void write_com(uchar com);
+void write_data(uchar dat);
+void init_lcd12864(void);
+void write_string(uchar x,uchar y,char *z);
+
+
+
+#endif
